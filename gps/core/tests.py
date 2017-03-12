d8e703b75480903ab5834d8791f04d17d89713e1
Marcelo Belli <marcelo@belli.me>
2017-03-11 23:37:54 -0300

    Basic view now returns minimal HTML

--- gps/core/tests.py
+++ gps/core/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
 from gps.core.views import home_page
 
@@ -9,3 +10,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>GPS</title>', html)
+        self.assertTrue(html.endswith('</html>'))
